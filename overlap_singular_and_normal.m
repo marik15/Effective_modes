3b62d1a41dab3a25d61d7b15eebea04ca1c7e274
Marat <zakirov.mn16@physics.msu.ru>
2022-12-27 12:53:45 +0300

    Overlap singular and normal

--- overlap_singular_and_normal.m
+++ overlap_singular_and_normal.m
@@ -1,9 +1,23 @@
 % Программа создаёт видеоанимацию (пока убрано) и .txt-файл с матрицами перекрываний
 
 path = 'C:\MATLAB\Эффективные моды\';  %  папка с irc-файлами, в конце символ \
-files = {'w5_4long.irc'};  %  имена файлов
+files = {'w3_1.irc';
+         'w3_2.irc';
+         'w3_3.irc';
+         'w4_1.irc';
+         'w4_2.irc';
+         'w4_3.irc';
+         'w5_1.irc';
+         'w5_2.irc';
+         'w5_3.irc';};
+         %'w5_4long.irc'};  %  имена файлов
+path_normal = 'C:\MATLAB\Эффективные моды\normal_modes\';  %  папка, с файлами с нормальными модами
+files_normal = {'w3.mp2';
+                'w4.mp2';
+                'w5.mp2'};  %  имена файлов с нормальными модами
 L = 4000;  %  длина интервала в отсчетах, по которому считаем вектор
-start_arr = [1, 4001, 40001, 56001, 60001, 64001, 68001, 80001, 120001, 140001, 160001];  %  массив начал
+%start_arr = [1, 4001, 40001, 56001, 60001, 64001, 68001, 80001, 120001, 140001, 160001];  %  массив начал
+start_arr = 1:L:96001;
 end_arr = start_arr + L - 1;  %  массив концов
 
 % --- ниже не нужно редактировать
@@ -14,6 +28,19 @@ sample = [cd, '\wx.sample'];
 
 t1 = 1;  %  начало траектории - целое число
 t2 = 'end';  %  конец траектории - целое число, либо слово 'end', если нужно посчитать до конца файла, но число строк неизвестно
+
+for k = 1:numel(files_normal)
+    filename_normal = append(path_normal, files_normal{k});
+    M = read_modes(filename_normal);
+    switch (size(M, 1)/(3*3))
+        case 3
+            w3 = M;
+        case 4
+            w4 = M;
+        case 5
+            w5 = M;
+    end
+end
 
 for k = 1:numel(files)
     file = files{k};
@@ -43,26 +70,27 @@ for k = 1:numel(files)
         N = size(E12, 2)/3;
     end
 
+    switch N/3
+        case 3
+            w = w3;
+        case 4
+            w = w4;
+        case 5
+            w = w5;
+    end
+
     T = numel(start_arr) - 1;
     A = zeros(T, 3*N, 3*N);
     for t = 1:T
         [~, ~, V1] = svd(E12(start_arr(t):end_arr(t), :), 0);
-        [~, ~, V2] = svd(E12(start_arr(t+1):end_arr(t+1), :), 0);
         for e1 = 1:3*N
             for e2 = 1:3*N
-                A(t, e1, e2) = dot(V1(:, e1), V2(:, e2));
+                A(t, e1, e2) = dot(V1(:, e1), w(:, e2));
             end
         end
     end
 
-    %{
-    path_video = append(path, 'Видео\');
-    if (~isfolder(path_video))
-        mkdir(path_video);  %  создание папки с видео
-    end
-    draw_overlap_video(abs(A), start_arr, end_arr, path_video, name);
-    %}
-    path_matrix = append(path, 'Матрицы\');
+    path_matrix = append(path, 'Матрицы сингулярные-нормальные\');
     if (~isfolder(path_matrix))
         mkdir(path_matrix);  %  создание папки с таблицами
     end
