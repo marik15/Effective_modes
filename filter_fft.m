bed24de3ab2fecfa8693d6398debf6ff37b4e0b9
Marat <zakirov.mn16@physics.msu.ru>
2023-06-24 23:59:44 +0300

    filter has been developed

--- filter_fft.m
+++ filter_fft.m
@@ -1,13 +1,8 @@
-function [freq, P1] = filter_fft(sig, fs, range)
-    Y = fft(sig);
-    L = size(sig, 2);
-    P2 = abs(Y/L);
-    P1 = P2(1:fix(L/2+1));
-    P1(2:end-1) = 2*P1(2:end-1);
+function sig_filtered = filter_fft(sig, fs, range)
+    L = length(sig);
     freq = fs*(0:(L/2))/L;
-
-    P1 = zeros(size(Y));
-    P1(2:L/2) = 0.5*P1(2:end);
-    P1(L/2:end) = 0.5*P1(2:end);  %  check boundaries
-    P1 = ifft(P1);
+    mask = (freq >= range(1)) .* (freq <= range(end));  %  оставляем индексы, которые соответствуют частотному диапазону
+    Y = fft(sig);
+    Y = Y.*[mask, mask(end:-1:2)];
+    sig_filtered = ifft(Y);
 end
