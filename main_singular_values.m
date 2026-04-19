dcaae9d1157fffe7f07c082101415048f53d869a
marik15 <zakirov1998@list.ru>
2026-04-19 13:06:16 +0300

    Bug fixed

--- main_singular_values.m
+++ main_singular_values.m
@@ -2,7 +2,7 @@
 
 path_data = 'D:\MATLAB\Ёффективные моды\data\';  %  папка с файлами, в конце символ \
 files = {'w3_1.irc'};  %  имена файлов
-t_step = 5000;  %  шаг, отсчеты
+t_step = 100000;  %  шаг, отсчеты
 t1 = 1;  %  начало траектории
 t2 = 'end';  % конец траектории: целое число, либо слово 'end', если нужно посчитать до конца файла, а число строк неизвестно
 
@@ -32,13 +32,14 @@ for k = 1:numel(files)
         E12 = energy_power(qVxyz, 0.5);
         s = svd(E12 - mean(E12), 0);  %  сингул€рные числа в пор€дке убывани€
 
-        s = s.^2 * sqrt((1e+4) / size(U, 1) / 4.1868);  %  энерги€, ккал/моль
-        plot(ax, s);  %  построение графика
+        s = s.^2 * sqrt((1e+4) / size(E12, 1) / 4.1868);  %  энерги€, ккал/моль
+        plot(ax, s/sum(s), 'LineWidth', 2, 'Marker', 'square');  %  построение графика
         xlim(ax, [1, 3*n]);
-        title(ax, ['«ависимость квадратов сингул€рных чисел от их номера дл€ файла ', files{k}, ' Time ', num2str(t1_cur), '-', num2str(t2_cur)], 'FontSize', 16, 'Interpreter', 'none');
-        xlabel(ax, 'Ќомер числа', 'FontSize', 14);
-        ylabel(ax, ' вадрат сингул€рного числа, пересчитанный в ккал/моль', 'FontSize', 14);
-        saveas(fig, [output_path, '√рафик SVD дл€ ', files{k}, ' ', num2str(t1_cur), '-', num2str(t2_cur), '.jpg']);
+        set(ax, 'FontSize', 40);
+        title(ax, ['«ависимость квадратов сингул€рных чисел от их номера дл€ файла ', files{k}, ' Time ', num2str(t1_cur), '-', num2str(t2_cur)], 'FontSize', 24, 'Interpreter', 'none');
+        xlabel(ax, 'Ќомер числа', 'FontSize', 24);
+        ylabel(ax, ' вадрат сингул€рного числа, пересчитанный в ккал/моль', 'FontSize', 24);
+        saveas(fig, [output_path, '√рафик SVD дл€ ', files{k}, ' ', num2str(t1_cur), '-', num2str(t2_cur), '.png']);
         writematrix(s, [output_path, '„исла SVD дл€ ', files{k}, ' ', num2str(t1_cur), '-', num2str(t2_cur), '.txt'], 'Delimiter', 'space');  %  запись сингул€рных значений в текстовый файл
     end
 end
